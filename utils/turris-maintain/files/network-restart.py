58e2d67b32d8b8b6d60755813f1dc0b892430185
Martin Matějek <martin.matejek@nic.cz>
2022-10-06 20:30:49 +0200

    WIP: turris-maintain: refactor python scripts to simple modules
    [Copy changelog here...]

--- utils/turris-maintain/files/network-restart.py
+++ utils/turris-maintain/files/network-restart.py
@@ -4,61 +4,103 @@ import time
 import json
 import re
 import subprocess
+from typing import NoReturn
 import euci
 
 from foris_controller.buses.mqtt import MqttNotificationSender
 
-TIME = 2000
-STEP = 200
+
+class BaseClass:
+    TIME = 2000
+    STEP = 200
+    NOTIFY_EVENT = None  # redefine it in child classes
+
+    def __init__(self):
+        self.uci = euci.EUci()
+        try:
+            self.controller_id = subprocess.check_output(
+                ["crypto-wrapper", "serial-number"]).decode().strip()
+        except subprocess.CalledProcessError:
+            self.controller_id = None
+
+        self._get_mqtt_config_vars()
+        self.sender = MqttNotificationSender(self.host, self.port, self.credentials)
+
+    def _get_mqtt_config_vars(self):
+        self.host = self.uci.get("foris-controller", "mqtt", "host", default="localhost")
+        self.port = self.uci.get("foris-controller", "mqtt", "port", dtype=int, default=11883)
+        self.passwd_path = self.uci.get(
+            "foris-controller", "mqtt", "credentials_file", default="/etc/fosquitto/credentials.plain"
+        )
+        with open(self.passwd_path, "r") as f:
+            self.credentials = re.match(r"^([^:]+):(.*)$", f.readlines()[0][:-1]).groups()
+
+    def _notify_progress(self, msg=None):
+        remains = self.TIME
+        notification_msg = {"remains": remains}
+        if msg:
+            notification_msg.update(msg)
+
+        while remains > 0:
+            self.sender.notify(
+                "maintain", self.NOTIFY_EVENT, notification_msg,
+                controller_id=self.controller_id,
+            )
+            time.sleep(float(self.STEP) / 1000)
+            remains -= self.STEP
+
+        # how to handle the final {"remains": 0}
+        self.sender.notify(
+            "maintain", self.NOTIFY_EVENT, notification_msg, controller_id=self.controller_id,
+        )
+
+    def _run_final_action(self) -> NoReturn:
+        """Run action (e.g. service reload) after sending notification."""
+        raise NotImplementedError
+
+    def run(self) -> NoReturn:
+        """Define all steps that needs to be run for maintain-<something>."""
+        raise NotImplementedError
 
 
-uci = euci.EUci()
-controller_id = None
+class NetworkClass(BaseClass):
+    def __init__(self):
+        self.ips = []
+        super().__init__()
 
-host = uci.get("foris-controller", "mqtt", "host", default="localhost")
-port = uci.get("foris-controller", "mqtt", "port", dtype=int, default=11883)
-passwd_path = uci.get(
-    "foris-controller", "mqtt", "credentials_file", default="/etc/fosquitto/credentials.plain"
-)
-try:
-    controller_id = subprocess.check_output(
-        ["crypto-wrapper", "serial-number"]).decode().strip()
-except subprocess.CalledProcessError:
-    controller_id = None
-with open(passwd_path, "r") as f:
-    credentials = re.match(r"^([^:]+):(.*)$", f.readlines()[0][:-1]).groups()
-sender = MqttNotificationSender(host, port, credentials)
+    def _get_router_ips(self):
+        # try to detect ips from uci
+        # parse ips as if they were in CIDR notation
+        self.ips += [e.split("/")[0] for e in self.uci.get("network", "lan", "ipaddr", list=True, default=()) if e]
+        self.ips += [e.split("/")[0] for e in self.uci.get("network", "lan", "ip6addr", list=True, default=()) if e]
+        self.ips += [e.split("/")[0] for e in self.uci.get("network", "wan", "ipaddr", list=True, default=()) if e]
+        self.ips += [e.split("/")[0] for e in self.uci.get("network", "wan", "ip6addr", list=True, default=()) if e]
 
-ips = []
-# try to detect ips from uci
-# parse ips as if they were in CIDR notation
-ips += [e.split("/")[0] for e in uci.get("network", "lan", "ipaddr", list=True, default=()) if e]
-ips += [e.split("/")[0] for e in uci.get("network", "lan", "ip6addr", list=True, default=()) if e]
-ips += [e.split("/")[0] for e in uci.get("network", "wan", "ipaddr", list=True, default=()) if e]
-ips += [e.split("/")[0] for e in uci.get("network", "wan", "ip6addr", list=True, default=()) if e]
+        # try to detect_ips from ubus
+        for network in ["lan", "wan"]:
+            try:
+                output = subprocess.check_output(["ifstatus", network])
+                data = json.loads(output)
+                self.ips += [e["address"] for e in data.get("ipv4-address", [])]
+                self.ips += [e["address"] for e in data.get("ipv6-address", [])]
+            except (subprocess.CalledProcessError, ):
+                pass
 
-# try to detect_ips from ubus
-for network in ["lan", "wan"]:
-    try:
-        output = subprocess.check_output(["ifstatus", network])
-        data = json.loads(output)
-        ips += [e["address"] for e in data.get("ipv4-address", [])]
-        ips += [e["address"] for e in data.get("ipv6-address", [])]
-    except (subprocess.CalledProcessError, ):
-        pass
+# in the actual maintain-something module
+class NetworkRestart(NetworkClass):
+    TIME = 3000
+    STEP = 200
+    NOTIFY_EVENT = "network-restart"
+    SERVICE_NAME = "network"
+
+    def _run_final_action(self):
+        subprocess.call([f"/etc/init.d/{self.SERVICE_NAME}", "reload"])
+
+    def run(self):
+        self._get_router_ips()
+        self._notify_progress()
+        self._run_final_action()
 
 
-remains = TIME
-while remains > 0:
-    sender.notify(
-        "maintain", "network-restart", {"ips": list(set(ips)), "remains": remains},
-        controller_id=controller_id,
-    )
-    time.sleep(float(STEP) / 1000)
-    remains -= STEP
-
-sender.notify(
-    "maintain", "network-restart", {"ips": ips, "remains": 0}, controller_id=controller_id,
-)
-
-subprocess.call(["/etc/init.d/network", "reload"])
+network_restart = NetworkRestart()
+network_restart.run()
