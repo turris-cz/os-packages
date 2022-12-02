0c475d3068b32ac1b6630aa9107c0fa403ffde3c
Michal Vasilek <michal.vasilek@nic.cz>
2022-12-02 14:08:51 +0100

    schnapps: include branch name in auto-snapshots
    schnapps creates a snapshot before and after every update and includes
    the Turris OS version there, but sometimes the Turris OS version does not
    contain enough information.
    
    For example when switching between hbk, hbs and crashlab (based on hbk),
    I would like to immediately see where I should rollback if I want to go to
    an older snapshot of hbk.

--- utils/schnapps/files/updater_preupdate.sh
+++ utils/schnapps/files/updater_preupdate.sh
@@ -2,4 +2,5 @@
 [ "$ROOT_DIR" = "/" ] || exit 0
 [ "$(findmnt -nf -o FSTYPE /)" = "btrfs" ] || exit 0
 source /etc/os-release
-schnapps create -t pre "Automatic pre-update snapshot ($PRETTY_NAME)"
+branch="$(uci get updater.turris.branch)"
+schnapps create -t pre "Automatic pre-update snapshot ($PRETTY_NAME - $branch)"
