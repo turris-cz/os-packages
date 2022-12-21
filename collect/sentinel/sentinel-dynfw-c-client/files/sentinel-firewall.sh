c72f391d81c93e7ce94fb3521514eab3fde721b5
Michal Hrusecky <michal.hrusecky@turris.com>
2022-12-21 16:43:53 +0100

    sentinel-dynfw-c-client: Add new package
    Add alternative Sentinel Dynfw client written in C.

---
+++ collect/sentinel/sentinel-dynfw-c-client/files/sentinel-firewall.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+. "${0%/*}/common.sh"
+. /lib/functions.sh
+
+IPSET="turris-sn-dynfw-block"
+
+# Always create IP set to prevent iptables error about missing ipset.
+ipset create "$IPSET"_v4 hash:ip family inet -exist
+ipset create "$IPSET"_v6 hash:net family inet6 -exist
+
+dynfw_block() {
+	local config_section="$1"
+	local zone enabled
+	config_get zone "$config_section" "name"
+	config_get_bool enabled "$config_section" "sentinel_dynfw" "0"
+	[ "$enabled" = "1" ] || return 0
+
+	report_operation "Dynamic blocking on zone '$zone'"
+	for chain in "input" "forward"; do
+		local chain_enabled
+		config_get_bool chain_enabled "$config_section" "sentinel_dynfw_${chain}" "1"
+		[ "$chain_enabled" = "1" ] || continue
+		report_info "$chain"
+
+		bypass_mark=""
+		[ "${chain}" == "input" ] && bypass_mark="-m mark ! --mark 0x10/0x10"
+
+		iptables_drop "${zone}" "${chain}" \
+			-m set --match-set "$IPSET"_v4 src \
+			${bypass_mark} \
+			-m conntrack --ctstate NEW \
+			-m comment --comment "!sentinel: dynamic firewall block"
+
+		ip6tables_drop "${zone}" "${chain}" \
+			-m set --match-set "$IPSET"_v6 src \
+			${bypass_mark} \
+			-m conntrack --ctstate NEW \
+			-m comment --comment "!sentinel: dynamic firewall block"
+
+	done
+}
+
+config_load "firewall"
+config_foreach dynfw_block "zone"
