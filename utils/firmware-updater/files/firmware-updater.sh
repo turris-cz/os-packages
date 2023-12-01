787ece0b362867a2d06c84e9ca9207b78676b607
Michal Hrusecky <michal.hrusecky@turris.com>
2023-12-01 14:42:26 +0100

    firmware-updater: Fix schnapps integration
    Provide output descriptor to schnapps and eliminate one cat.

--- utils/firmware-updater/files/firmware-updater.sh
+++ utils/firmware-updater/files/firmware-updater.sh
@@ -2,4 +2,4 @@
 [ -z "$ROOT_DIR" ] || [ "$ROOT_DIR" = "/" ] || exit 0
 nor-update
 [ ! -x /usr/bin/omnia-mcutool ] || omnia-mcutool --upgrade
-[ "$(schnapps factory-version | cut -f 1 -d .)"  -eq "$(cat /etc/turris-version | cut -f 1 -d .)" ] || schnapps update-factory
+[ "$(schnapps factory-version | cut -f 1 -d .)"  -eq "$(cut -f 1 -d . /etc/turris-version)" ] || schnapps update-factory > /dev/null 2>&1
