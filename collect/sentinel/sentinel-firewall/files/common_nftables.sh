04cf4eb22c83fdb43abbbbe1185472b525c1d2fb
Tomas Zak <tomas.zak@turris.com>
2024-06-07 22:19:00 +0200

    turrispackages: sentinel: minipots on nftable
    Test build

--- collect/sentinel/sentinel-firewall/files/common_nftables.sh
+++ collect/sentinel/sentinel-firewall/files/common_nftables.sh
@@ -1,5 +1,7 @@
 # These are common helpers for sentinel-firewall scripts.
 
+
+
 report_operation() {
 	echo "   *" "$@" >&2
 }
@@ -33,6 +35,20 @@ iptables_nat_chain_exists() {
 ip6tables_nat_chain_exists() {
 	ip6tables-legacy -t nat -S "$1" >/dev/null 2>&1
 }
+
+# This is simple helper to check for existence of given chain
+nftables_portfw_chain_exists() {
+    nft list chain inet fw4 input_wan | grep "ct status dnat" >/dev/null 2>&1
+}
+
+# This is simple helper to check for existence of given chain in nat table
+nftables_dnat_chain_exists() {
+    nft list chain inet fw4 "dstnat_$1" >/dev/null 2>&1
+    echo $1
+}
+
+
+
 
 # Add drop rule
 # zone: name of firewall zone incoming packets are coming from
@@ -95,3 +111,43 @@ iptables_redirect() {
 			-j REDIRECT --to-ports "$local_port"
 	fi
 }
+
+# Add port forwarding rule
+# zone: name of firewall zone incoming packets are coming from
+# chain: chain to be affected (input/forward)
+# Any additional arguments are passed to iptables call adding this rule
+nftables_set_portfw() {
+    local wan_if=$(nft list chain inet fw4 reject_from_wan | grep iifname | grep -Eo "\".*?[0-9]\"")
+    
+    # recreates it if it is missing.
+    if ! nftables_portfw_chain_exists; then
+        nft insert rule inet fw4 input_wan ct status dnat counter accept \
+        comment "\"!Sentinel-minipots enable port forwarding\""
+
+        nft add chain inet fw4 dstnat_wan
+
+        nft add rule inet fw4 dstnat meta iifname { $wan_if } \
+        counter jump dstnat_wan comment "\"!Sentinel-minipots forward for interface(s) in wan zone meta packets to dstnat_wan chain\""
+
+    fi
+
+}
+
+# Add port redirect rule.
+# zone: name of firewall zone incoming packets are coming from
+# port: target port of packet
+# local_port: local port to redirect packet to
+# description: description for this redirect printed and recorded in comment
+port_redirect() {
+    local zone="$1"
+    local port="$2"
+    local local_port="$3"
+    local description="$4"
+
+    report_operation "$description on zone '$zone' ($port -> $local_port)"
+    if nftables_dnat_chain_exists; then
+        nft add rule inet fw4 "dstnat_$zone" meta nfproto { ipv4, ipv6 } counter \
+        tcp dport $port redirect to $local_port comment "\"!sentinel: $description port redirect\""
+    fi
+}                                                                                                           
+
