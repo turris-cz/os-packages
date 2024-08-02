99d72515f9c95a21208ac9b1103ab86540e0b331
Michal Hrusecky <michal.hrusecky@turris.com>
2024-08-02 14:46:37 +0200

    sentinel-fwlogs: Adapt to support nftables

---
+++ collect/sentinel/sentinel-fwlogs/files/sentinel-firewall-nftables.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+. "${0%/*}/common.sh"
+. /lib/functions.sh
+. /lib/functions/sentinel.sh
+. /usr/libexec/sentinel/fwlogs-defaults.sh
+
+allowed_to_run "fwlogs" 2>/dev/null || return 0
+
+
+config_load "sentinel"
+config_get nflog_group fwlogs nflog_group "$DEFAULT_NFLOG_GROUP"
+config_get nflog_threshold fwlogs nflog_threshold "$DEFAULT_NFLOG_THRESHOLD"
+
+
+fwlogs_logging() {
+	local config_section="$1"
+	local zone enabled
+	config_get zone "$config_section" "name"
+	config_get_bool enabled "$config_section" "sentinel_fwlogs" "0"
+	[ "$enabled" = "1" ] || return 0
+
+	report_operation "Logging of zone '$zone'"
+	nft insert rule inet fw4 "drop_from_${zone}" log group "$nflog_group" queue-threshold "$nflog_threshold"
+}
+
+config_load "firewall"
+config_foreach fwlogs_logging "zone"
