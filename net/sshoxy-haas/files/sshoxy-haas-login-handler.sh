fbeb792d6141f0fce4c03314d392f2657801270e
Stepan Henek <stepan.henek@nic.cz>
2025-02-07 09:24:59 +0100

    sshoxy-haas: package added
    Added new version of HAAS package, written in Rust language. Initial relase.

---
+++ net/sshoxy-haas/files/sshoxy-haas-login-handler.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+. /usr/share/libubox/jshn.sh
+
+peer="$1"
+user="$2"
+password="$3"
+
+[ -n "$peer" -o -n "$user" ]  || exit 1  # password can be empty
+
+echo "Ower #$peer# #$user# #$password#"
+
+
+# notify foris-controller
+json_init
+echo afdsdsafsaf
+echo "Ower #$peer# #$user# #$password#"
+json_add_string peer "$peer"
+echo "Ower #$peer# #$user# #$password#"
+json_add_string user "$user"
+echo "Ower #$peer# #$user# #$password#"
+json_add_string password "$password"
+echo "Ower #$peer# #$user# #$password#"
+
+echo /usr/bin/foris-notify-wrapper -m haas -a login_attempt "$(json_dump)"
