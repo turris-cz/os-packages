17058223a57fb936a7b14aeed7b7de67ed4f5cba
Lukas Jelinek <lukas.jelinek@nic.cz>
2025-04-15 16:18:23 +0200

    fixup! common_passwords: Add automatic update of password lists

--- utils/common_passwords/files/update-common-passwords.sh
+++ utils/common_passwords/files/update-common-passwords.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+set -e
+
+TMPDIR="/tmp"
 CPDIR="/usr/share/common_passwords"
 
-curl -o "$CPDIR/sentinel_passwords.tmp" https://view.sentinel.turris.cz/common_passwords/sentinel_passwords
-mv "$CPDIR/sentinel_passwords.tmp" "$CPDIR/sentinel_passwords"
 
-curl -o "$CPDIR/haas_passwords.tmp" https://view.sentinel.turris.cz/common_passwords/haas_passwords
-mv "$CPDIR/haas_passwords.tmp" "$CPDIR/haas_passwords"
+# Sentinel passwords
+
+# Download list from server
+curl -o "$TMPDIR/sentinel_passwords.tmp" https://view.sentinel.turris.cz/common_passwords/sentinel_passwords 2> /dev/null
+
+# Update if new list differs
+cmp "$TMPDIR/sentinel_passwords.tmp" "$CPDIR/sentinel_passwords" > /dev/null || mv "$TMPDIR/sentinel_passwords.tmp" "$CPDIR/sentinel_passwords"
+
+
+# HaaS passwords
+
+# Download list from server
+curl -o "$TMPDIR/haas_passwords.tmp" https://view.sentinel.turris.cz/common_passwords/haas_passwords 2> /dev/null
+
+# Update if new list differs
+cmp "$TMPDIR/haas_passwords.tmp" "$CPDIR/haas_passwords" > /dev/null || mv "$TMPDIR/haas_passwords.tmp" "$CPDIR/haas_passwords"
