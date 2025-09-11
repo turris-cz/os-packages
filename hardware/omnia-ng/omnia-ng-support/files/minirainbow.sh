782211a19f3b11ae51e957062438a15b5ffc6f6d
Michal Hrusecky <michal.hrusecky@turris.com>
2025-09-11 11:27:59 +0200

    omnia-ng-support: support brightness in minirainbow

--- hardware/omnia-ng/omnia-ng-support/files/minirainbow.sh
+++ hardware/omnia-ng/omnia-ng-support/files/minirainbow.sh
@@ -8,6 +8,15 @@ BLACK="0 0 0"
 
 WAN_LED="/sys/class/leds/rgb:wan"
 WIFI_LED="/sys/class/leds/rgb:wlan"
+POWER_LED="/sys/class/leds/rgb:power"
+
+TRIGGERS=no
+
+set_color() {
+    local color="$1"
+    local dev="$2"
+    set_val "$color" "$dev"/multi_intensity
+}
 
 set_trigger() {
     local dev="${2}"
@@ -21,8 +30,15 @@ set_val() {
     grep -q "$new_val" "$dev" || echo "$new_val" > "$dev"
 }
 
+set_brightness() {
+    BRIGHTNESS="$(uci get rainbow.all.brightness 2> /dev/null)"
+    for i in /sys/class/leds/*; do
+        echo "$BRIGHTNESS" > "$i/brightness"
+    done
+}
+
 wan_status() {
-    if uci show systemn | grep '^system.@led[0-9*].sysfs=.rgb:wan.'; then
+    if uci show systemn 2> /dev/null | grep '^system.@led[0-9*].sysfs=.rgb:wan.'; then
         return 0;
     fi
     local connectivity="$(check_connection)"
@@ -42,18 +58,23 @@ wan_status() {
             status="$RED"
         fi
     fi
-    set_trigger "netdev" "$WAN_LED"/trigger
-    set_val "$status" "$WAN_LED"/multi_intensity
-    set_val "$(ifstatus wan | jsonfilter -e '@.device')" "$WAN_LED"/device_name
-    set_val 1 "$WAN_LED"/link
-    set_val 1 "$WAN_LED"/rx
-    set_val 1 "$WAN_LED"/tx
+    set_color "$status" "$WAN_LED"
+    if [ "$TRIGGERS" = yes ]; then
+        set_trigger "netdev" "$WAN_LED"/trigger
+        set_val "$(ifstatus wan | jsonfilter -e '@.device')" "$WAN_LED"/device_name
+        set_val 1 "$WAN_LED"/link
+        set_val 1 "$WAN_LED"/rx
+        set_val 1 "$WAN_LED"/tx
+    else
+        set_trigger "default-on" "$WAN_LED"/trigger
+    fi
+    set_brightness
     [ "$status" = "$GREEN" ] || [ "$status" = "$CYAN" ]
     return $?
 }
 
 wifi_status() {
-    if uci show systemn | grep '^system.@led[0-9*].sysfs=.rgb:wlan.'; then
+    if uci show system 2> /dev/null | grep '^system.@led[0-9*].sysfs=.rgb:wlan.'; then
         return 0;
     fi
     local bands="$(wifi status | jsonfilter -e '$.*.config.band' | sort -u | wc -l)"
@@ -68,28 +89,85 @@ wifi_status() {
     else
         color="$ORANGE"
     fi
-    set_trigger "netdev" "$WIFI_LED"/trigger
-    set_val "$color" "$WIFI_LED"/multi_intensity
-    set_val "$(ifstatus lan | jsonfilter -e '@.device')"  "$WIFI_LED"/device_name
-    set_val 1 "$WIFI_LED"/link
-    set_val 1 "$WIFI_LED"/rx
-    set_val 1 "$WIFI_LED"/tx
+    set_color "$color" "$WIFI_LED"
+    if [ "$TRIGGERS" = yes ]; then
+        set_trigger "netdev" "$WIFI_LED"/trigger
+        set_val "$(ifstatus lan | jsonfilter -e '@.device')"  "$WIFI_LED"/device_name
+        set_val 1 "$WIFI_LED"/link
+        set_val 1 "$WIFI_LED"/rx
+        set_val 1 "$WIFI_LED"/tx
+    else
+        set_trigger "default-on" "$WIFI_LED"/trigger
+    fi
+    set_brightness
 }
 
-while true; do
-    if wan_status; then
-        sleep 300
-    else
-        sleep 10
-    fi
-done &
+BRIGHTNESS="$(uci get rainbow.all.brightness 2> /dev/null)"
+[ -n "$BRIGHTNESS" ] || BRIGHTNESS=255
+
+if ! uci show system 2> /dev/null | grep '^system.@led[0-9*].sysfs=.rgb:power.'; then
+    set_trigger "default-on" "$POWER_LED"/trigger
+    set_color "$GREEN" "$POWER_LED"
+    set_brightness
+fi
+
+uci show rainbow 2> /dev/null | grep -q 'rainbow.all.brightness' || {
+    touch /etc/onfig/rainbow
+    uci set rainbow.all=led
+    uci set rainbow.all.brightness="$BRIGHTNESS"
+    uci commit rainbow
+}
+
+{
+    trap "set_brightness;" HUP
+    set_brightness
+    down="false"
+    while true; do
+        key="$(head -c 48 /dev/input/event0 | hexdump -e '"%02x"' | tail -c +18 | head -c 2)"
+        if [ "$key" = 3b ]; then
+            if [ "$down" = true ]; then
+                down=false
+            else
+                down=true
+                if [ "$BRIGHTNESS" -lt 4 ]; then
+                    if [ "$BRIGHTNESS" -eq 0 ]; then
+                        BRIGHTNESS=255
+                    else
+                        BRIGHTNESS=0
+                    fi
+                else
+                    BRIGHTNESS="$((BRIGHTNESS / 2))"
+                fi
+                uci set rainbow.all.brightness="$BRIGHTNESS"
+                uci commit rainbow
+                set_brightness
+            fi
+        fi
+    done
+} &
+BRIGHTNESS_PID="$!"
+
+{
+    trap "wan_status;" HUP
+    while true; do
+        if wan_status; then
+            sleep 300
+        else
+            sleep 10
+        fi
+    done
+} &
 WAN_STATUS_PID="$!"
 
-while true; do
-    wifi_status
-    sleep 10
-done &
+{
+    trap "wifi_status;" HUP
+    while true; do
+        wifi_status
+        sleep 10
+    done
+} &
 WIFI_STATUS_PID="$!"
 
-trap "kill $WIFI_STATUS_PID; kill $WAN_STATUS_PID;" INT
+
+trap "kill $WIFI_STATUS_PID; kill $WAN_STATUS_PID; kill $BRIGHTNESS_PID;" INT
 wait
