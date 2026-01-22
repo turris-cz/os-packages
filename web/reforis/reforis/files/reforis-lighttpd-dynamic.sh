7bbe8dcf8d4aa4eb59e8978f5392106a0cfc1bce
Stepan Henek <stepan.henek@nic.cz>
2026-01-22 15:43:35 +0100

    reforis: remove flup
    reforis can use WSGIServer (intial setup) or eventlet (in production)

--- web/reforis/reforis/files/reforis-lighttpd-dynamic.sh
+++ web/reforis/reforis/files/reforis-lighttpd-dynamic.sh
@@ -1,13 +1,9 @@
-#!/bin/sh
+!/bin/sh
 
 . /lib/functions.sh
 
 config_load reforis
 config_get SCRIPTNAME server scriptname "/reforis"
-# config_get_bool DEBUG server debug "0"
-# config_get_bool NOAUTH auth noauth "0"
-config_get SESSION_TIMEOUT auth session_timeout ""
-config_get SENTRY_DSN sentry dsn ""
 
 # scriptname must not contain escape codes (avoid CRLF injection in sed later)
 # and for the sake of UX, trailing and leading slashes are trimmed
@@ -15,10 +11,6 @@ SCRIPTNAME=$(echo "$SCRIPTNAME" | sed -e 's;\\;\\\\;g' | sed -e 's;/*$;;g' | sed
 [ "$SCRIPTNAME" != "" ] && SCRIPTNAME="/$SCRIPTNAME"
 
 # get bus config
-config_load foris-controller
-config_get BUS_HOST mqtt host "localhost"
-config_get BUS_PORT mqtt port "11883"
-config_get CREDENTIALS_FILE mqtt credentials_file "/etc/fosquitto/credentials.plain"
 CONTROLLER_ID=$(crypto-wrapper serial-number)
 
 REFORIS_PATH=$(python -c 'from pathlib import Path;print(Path(__import__("reforis").__file__).parent.parent)')
@@ -32,27 +24,8 @@ echo " \$HTTP[\"url\"] =~ \"^\" + var.reforis.scriptname + \"/static/\" {"
 echo "  alias.url += ( var.reforis.scriptname + \"/static/\" => \"${REFORIS_PATH}/reforis_static/\" )"
 echo " } else {"
 echo "  server.max-read-idle = 90"
-echo '	fastcgi.debug = 0'
-echo '	fastcgi.server = ('
-echo "	var.reforis.scriptname => ("
-echo '		"python-fcgi" => ('
-echo "			\"socket\" => \"/tmp/fastcgi.reforis.socket\","
-echo "			\"bin-path\" => var.reforis.bin,"
-echo '			"check-local" => "disable",'
-echo '			"idle-timeout" => 180,'
-echo '			"min-procs" => 0,'
-echo '			"max-procs" => 1,'
-echo '			"bin-environment" => ('
-if [ -n "$SENTRY_DSN" ]; then
-    echo "			\"SENTRY_DSN\" => \"$SENTRY_DSN\","
-fi
-echo "			\"CONTROLLER_ID\" => \"$CONTROLLER_ID\","
-echo '			),'
-echo '		),'
-echo '	turris_auth_scriptname => turris_auth'
-echo '	)'
-echo ')'
+echo '  fastcgi.debug = 0'
+echo '  fastcgi.server = ( "/" => ( turris_auth_scriptname => turris_auth ))'
+echo '  proxy.server = ( "" => ( ( "host" => "/var/run/reforis.sock" ) ) )'
 echo " }"
 echo "}"
-
-
