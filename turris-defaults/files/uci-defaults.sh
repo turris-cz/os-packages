316569719a1561f6750bdddead472ce45339b38d
Tomas Zak <tomas.zak@turris.com>
2026-09-11 16:59:11 +0200

    turris-defaults: change the way of serving default values
    TEST
    Due to changes in upstream we have to reflect them and change the way how serve
    default timezone and zonename.

--- turris-defaults/files/uci-defaults.sh
+++ turris-defaults/files/uci-defaults.sh
@@ -1,8 +1,13 @@
-if ! uci -q get 'system.@system[-1].zonename'; then
+zonename="$(uci -q get 'system.@system[-1].zonename')"
+timezone="$(uci -q get 'system.@system[-1].timezone')"
+country="$(uci -q get 'system.@system[-1]._country')"
+wizard_finished="$(uci -q get 'foris.wizard.finished')"
+
+if [ "$wizard_finished" != "1" ] && { [ -z "$zonename" ] || { [ "$zonename" = "UTC" ] && [ "$timezone" = "GMT0" ]; }; }; then
 	uci set 'system.@system[-1].zonename=Europe/Prague'
 fi
 
-if ! uci -q get 'system.@system[-1]._country'; then
+if [ -z "$country" ]; then
 	uci set 'system.@system[-1]._country=CZ'
 fi
 
